16958c249332612f1a69c57261e9bbb806cfbfdd
Karen <karen_vivianasis@hotmail.com>
2019-03-05 21:45:42 -0500

    lo ultimo

--- Inventario/controllers/usecontrollers.js
+++ Inventario/controllers/usecontrollers.js
@@ -2,7 +2,12 @@ var mysql = require('mysql');
 
 module.exports = {
      
-     getSignUp : function (rep, res, next) {
+     getSignUp: function (req, res, next) {
           return res.render('auth/signup')
+     },
+
+     postSignUp: function (req, res, next){
+          console.log(req.body.todo);
+          return;
      }
-}
+}; 
--- Inventario/routes/routes.js
+++ Inventario/routes/routes.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var controllers = require('../controllers')
+var controllers = require('.././controllers')
 
 router.get('/', controllers.HomeController.index);
 
 //ruta usuario
 
-router.get('/auth/signup', controllers.usecontrollers.getSignUp);
+router.post('/auth/signup', controllers.usecontrollers.getSignUp);
+router.get('/auth/signup', controllers.usecontrollers.postSignUp);
 
 module.exports = router;
