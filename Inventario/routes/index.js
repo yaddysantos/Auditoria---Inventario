2574d852e01c959771340cd09445633f15cba613
Karen <karen_vivianasis@hotmail.com>
2019-03-06 16:31:50 -0500

    arreglos

--- Inventario/routes/index.js
+++ Inventario/routes/index.js
@@ -11,4 +11,25 @@ router.get('/', (req, res,next) =>{
 router.get('/auditor/nuevo', (req, res,next) =>{
      res.render('auditor/nuevoa', {});
 });
+
+router.get('/auditor/nuevo', (req, res,next) =>{
+     res.render('auditor/nuevoa', {});
+});
+
+router.get('/formulario/c1', (req, res,next) =>{
+     res.render('auditor/C1', {});
+});
+
+router.get('/formulario/c2', (req, res,next) =>{
+     res.render('auditor/C2', {});
+});
+
+router.get('/formulario/c3', (req, res,next) =>{
+     res.render('auditor/C3', {});
+});
+
+router.get('/formulario/c4', (req, res,next) =>{
+     res.render('auditor/C4', {});
+});
+
 module.exports = router;
